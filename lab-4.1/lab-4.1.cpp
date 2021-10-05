8db3c21b1fb7a0171eb1f5c83823bce1eda7afc0
RIO489 <oleksander.hamaiunov.it.2021@lpnu.ua>
2021-10-05 17:07:39 +0300

    lab-4.1.1

---
+++ lab-4.1/lab-4.1.cpp
@@ -0,0 +1,53 @@
+﻿// Lab-4,1.cpp
+// < Гамаюнов Олександр >
+// Лабораторна робота № 4.1
+// Цикли.
+// Варіант 5.1
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	// 1 спосіб
+
+	int i;
+	double S;
+	S = 0;
+	i = 1;
+	while (i <= 15)
+	{
+		S += (sin(1. * i) * sin(1. * i)) + (cos(1. / i) * cos(1. / i)) / i * i;
+		i++;
+	}
+	cout << S << endl;
+	
+	// 2 спосіб
+
+		S = 0;
+	i = 1;
+	do {
+		S += (pow(sin(1. * i), 2) + pow(cos(1. / i), 2)) / i * i;
+		i++;
+	} while (i <= 15);
+	cout << S << endl;
+
+	// 3 спосіб
+	
+	S = 0;
+	for (i = 1; i <= 15; i++)
+	{
+		S += (pow(sin(1. * i), 2) + pow(cos(1. / i), 2)) / i * i;
+	}
+	cout << S << endl;
+
+	// 4 спосіб
+
+	S = 0;
+	for (i = 15; i >= 1; i--)
+	{
+		S += (pow(sin(1. * i), 2) + pow(cos(1. / i), 2)) / i * i;
+	}
+	cout << S << endl;
+	
+	return 0;
+}
